48b993aaa1c8979b49f63581b234d3b96ee16913
yanduanyin <1097720997@qq.com>
2019-09-04 23:52:45 +0800

    vue 第一个 element-ui 框架写的页面

---
+++ vue/vue-element/src/common/Header.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="header">
+    <!-- 头部的header -->
+    <div class="logo">后台管理系统</div>
+    <div class="user-info">
+      <el-dropdown>
+        <span class="el-dropdown-link">
+          {{userName}}<i class="el-icon-arrow-down el-icon--right"></i>
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>退出</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  computed: {
+    userName () {
+      return localStorage.getItem('userName')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.header {
+  background-color: #409EFF;
+  position: relative;
+  box-sizing: border-box;
+  width: 100%;
+  height: 70px;
+  font-size: 22px;
+  line-height: 70px;
+  color: #fff;
+}
+.header .logo {
+  float: left;
+  width: 250px;
+  text-align: center;
+}
+.user-info {
+  float: right;
+  padding-right: 50px;
+  font-size: 16px;
+  color: #fff;
+}
+.user-info .el-dropdown-link {
+  position: relative;
+  display: inline-block;
+  padding-left: 50px;
+  color: #fff;
+  cursor: pointer;
+  vertical-align: middle;
+}
+.user-info .user-logo {
+  position: absolute;
+  left: 0;
+  top: 15px;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+}
+.el-dropdown-menu__item {
+  text-align: center;
+}
+</style>
---
+++ vue/vue-element/src/components/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="main-content">
+    <!-- 头部的header -->
+    <!-- //使用组件 -->
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+// 引入组件
+import Header from '@/common/Header'
+export default {
+  name: 'mainContent',
+  data () {
+    return {
+    }
+  },
+  components: {// 声明组件
+    Header
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
