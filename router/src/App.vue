d675a4d1b8f7cc178c072f601ad73314b47d73d2
yanduanyin <1097720997@qq.com>
2019-09-05 22:43:50 +0800

    router 路由跳转 两种方式

---
+++ router/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <p>导航:
+      <router-link to="/">首页</router-link>
+      <router-link :to="{name: 'Look', params:{id: '我是以router-link 带入的id'}}">看一看</router-link>
+      <router-link to="/look/look1">看一</router-link>
+      <router-link to="/look/look2">看二</router-link>
+      <router-link to="/go-back">重定向</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ router/src/components/look.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="look">
+    <h1>{{ msg }}</h1>
+    {{$route.params.id}}<!-- 引入App.vue 传进来的参数 -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Look',
+  data () {
+    return {
+      msg: '看一看的页面'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
