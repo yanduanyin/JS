ce736585ca1676833eae3cb7319fc46f51d94fc9
yanduanyin <1097720997@qq.com>
2019-09-07 10:11:18 +0800

    ele 饿了么Vue App

---
+++ vue/vue-ele-app/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <v-header :seller="seller"></v-header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import header from '@/components/header/header.vue'
+export default {
+  name: 'App',
+  data () {
+    return {
+      seller: {}
+    }
+  },
+  components: {
+    'v-header': header
+  },
+  created () {
+    this.$http.get('https://www.easy-mock.com/mock/5d2ed7c57f168b5e40d35f45/qtitem/vue-eleme-seller_copy_1567759750894')
+      .then(res => {
+        console.log(res)
+        if (res.data.errno === 0) {
+          this.seller = Object.assign({}, this.seller, res.data.data)
+        }
+      })
+  }
+}
+</script>
+
+<style>
+
+</style>
