2bca0cfc8604095e184ef92c1e764a78b4fb5b06
yanduanyin <1097720997@qq.com>
2019-09-09 18:56:19 +0800

    vue 去哪儿讲解

---
+++ vue/qunar-app/src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import fastClick from 'fastclick'
+import App from './App'
+import router from './router'
+import './assets/style/iconfont/iconfont.css'
+import './assets/style/reset.css'
+
+Vue.config.productionTip = false
+fastClick.attach(document.body);//attach绑定一个事件
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ vue/qunar-app/src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <home-header></home-header>
+  </div>
+</template>
+<script>
+import HomeHeader from '../../components/Home-Head';
+export default {
+  components: {
+    // 'home-header': HomeHeader
+    HomeHeader,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
