fba772ff75f2bf21bf01cc0a5e55d9b9bfda683a
yanduanyin <1097720997@qq.com>
2019-09-16 19:05:11 +0800

    vue-components-communicate 组件通信

---
+++ vue/vue-components-communicate/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="child1">
+    <child1 :msg="msg" :msg2="msg2" :fn="appFn" v-bind="test" :count="count" @addCount="countAdd" ></child1>
+  </div>
+</template>
+
+<script>
+import child1 from '@/components/common/child1'
+
+export default {
+  name: 'App',
+  components: {
+    child1
+  },
+  data () {
+    return {
+      msg: '第一次给child1',
+      msg2: '第二次给child1',
+      test: {
+        a: 1,
+        b: 2
+      },
+      count: 1
+    }
+  },
+  methods: {
+    appFn () {
+      console.log('我是APP.vue 传给child1的方法')
+    },
+    countAdd () {
+      this.count++
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ vue/vue-components-communicate/src/components/common/child1.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{mag}}
+    {{msg2}}
+    <button @click="fn">按钮</button>
+    {{test.a}}
+    {{test.b}}
+    <button @click="add">+</button>
+    {{count}}
+  </div>
+</template>
+<script>
+export default {
+  props: ['mag', 'msg2', 'fn', 'test', 'count'],
+  methods: {
+    add () {
+      // this.count++   props过来的数据不可操作
+      this.$emit('addCount')
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
