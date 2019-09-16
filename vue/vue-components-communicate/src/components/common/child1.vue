7550da268da7b5e9136b40122d2f29a66a62baac
yanduanyin <1097720997@qq.com>
2019-09-16 20:46:08 +0800

    组件间通信

--- vue/vue-components-communicate/src/components/common/child1.vue
+++ vue/vue-components-communicate/src/components/common/child1.vue
@@ -3,15 +3,24 @@
     {{mag}}
     {{msg2}}
     <button @click="fn">按钮</button>
-    {{test.a}}
-    {{test.b}}
+    {{a}}
+    {{b}}
     <button @click="add">+</button>
     {{count}}
   </div>
 </template>
 <script>
 export default {
-  props: ['mag', 'msg2', 'fn', 'test', 'count'],
+  // props: ['mag', 'msg2', 'fn', 'a', 'b', 'count'],
+  // 如果父组件传值过来是用v-bind传过来的对象，那么子组件里用props接收必须要将对象里的key一一接收
+  props: {
+    a: {
+      type:Number
+    },
+    b: {
+      type:Number
+    }
+  },
   methods: {
     add () {
       // this.count++   props过来的数据不可操作
