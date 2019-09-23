31ef3ec03d990aeabb367b0f976db47aa1eabeb3
yanduanyin <1097720997@qq.com>
2019-09-23 16:13:33 +0800

    slide-search

---
+++ vue/slide-search/src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <!-- <SlideSearch></SlideSearch> -->
+    <slide-search :showSearchPage="showSearchPage"/>
+    <ul class="main">
+      <li>ziyin_666</li>
+      <li>ziyin_666</li>
+      <li>ziyin_666</li>
+      <li>ziyin_666</li>
+      <li>ziyin_666</li>
+      <li>ziyin_666</li>
+      <li>ziyin_666</li>
+      <li>ziyin_666</li>
+      <li>ziyin_666</li>
+      <li>ziyin_666</li>
+      <li>ziyin_666</li>
+      <li>ziyin_666</li>
+      <li>ziyin_666</li>
+      <li>ziyin_666</li>
+      <li>ziyin_666</li>
+      <li>ziyin_666</li>
+      <li>ziyin_666</li>
+      <li>ziyin_666</li>
+      <li>ziyin_666</li>
+      <li>ziyin_666</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import SlideSearch from './components/SlideSearch/index';
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      showSearchPage: false
+    }
+  },
+  components: {
+    SlideSearch
+  },
+  // 已经转化为真实 dom
+  mounted:function() {
+    const ul = document.querySelector('.main');
+    ul.addEventListener('scroll', () => {
+      if(ul.scrollTop === 0) {
+        this.showSearchPage = false;
+      }
+      if(ul.scrollTop > 10) {
+        this.showSearchPage = true;
+      }
+    })
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+ul {
+  height: calc(100vh - 94px);
+  overflow: scroll;
+}
+.main::-webkit-scrollbar {
+  display: none;
+}
+li {
+  margin-top: 20px;
+}
+</style>
---
+++ vue/slide-search/src/components/SlideSearch/index.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="search-wrap" :class="{'show-search':showSearchPage, 'hide-search': !showSearchPage}">
+    <div class="title-wrap" :class="{'show-search':showSearchPage}">
+      <span>标题</span>
+      <div>&gt;</div>
+    </div>
+    <div class="icon-back">
+      <span>&lt;</span>
+    </div>
+    <div class="search-input" :class="{'show-search':showSearchPage} ">
+      <input type="text" class="search" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props:{
+    showSearchPage:{
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+<style scoped>
+.search-wrap {
+  z-index: 999;
+  width: 100%;
+  height: 94px;
+  background-color: #fff;
+  transition: all .2s linear;
+}
+.title-wrap {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 42px;
+  text-align: center;
+  transition: all .2s linear;
+}
+.title-wrap div {
+  position: absolute;
+  height: 100%;
+  right: 0;
+  top: 0;
+  padding-right: 10px;
+}
+.icon-back {
+  position: absolute;
+  left: 10px;
+  top: 0;
+  z-index: 110;
+  height: 42px;
+}
+.search-input {
+  position: absolute;
+  top: 42px;
+  left: 0;
+  z-index: 100;
+  display: flex;
+  padding: 10px;
+  width: 100%;
+  box-sizing: border-box;
+}
+.search {
+  border: 1px solid #eee;
+  color: #666;
+  width: 100%;
+  height: 22px;
+  background: transparent;
+  font-size: 12px;
+  margin-left: 10px;
+  padding: 4px;
+  border-radius: 5px;
+  outline: none; 
+  /*outline: none;  去掉chrome浏览器自带的点击input框出现边框情况 */
+}
+.search-wrap.show-search {
+  height: 52px;
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, .1);
+}
+.search-wrap.hide-search {
+  box-shadow: none;
+}
+.search-input.show-search {
+  top: 0;
+  left: 40px;
+  transition: all .2s linear;
+}
+.title-wrap.show-search {
+  display: none;
+}
+</style>
