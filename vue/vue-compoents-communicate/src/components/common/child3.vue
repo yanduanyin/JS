db91d1b73638921fd9046d6a31a703148addae3b
yanduanyin <1097720997@qq.com>
2019-09-23 09:49:41 +0800

    vue-compoents-communicate

---
+++ vue/vue-compoents-communicate/src/components/common/child3.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="child3">
+    <p @click="change">{{msg}}</p>
+  </div>
+</template>
+
+<script>
+// import bus from '@/bus/index.js'
+export default {
+  data () {
+    return {
+      msg: '点击改变数据'
+    }
+  },
+  created () {
+    this.$bus.$on('toChild3', (param) => {
+      console.log(param)
+    })
+  },
+  beforeDestroy () { //销毁
+    this.$bus.$off('toChild3')
+  },
+  methods: {
+    change () {
+      this.$emit('fromChild', this.msg)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
