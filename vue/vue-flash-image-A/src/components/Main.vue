44ed08909a22d877b821161936f08b1df9e68541
yanduanyin <1097720997@qq.com>
2019-09-25 21:40:56 +0800

    vue-flash-image-A

---
+++ vue/vue-flash-image-A/src/components/Main.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main">
+    <div class="gauntlet">
+      <button @click="disAppear">消失</button>
+    </div>
+    <Content :heroes="heroes"/>
+  </div>
+</template>
+<script>
+import Content from './Content'
+import { heroes } from './heroes'
+export default {
+  data() {
+    return {
+      heroes: heroes
+    }
+  },
+  components: {
+    Content,
+    lostHeroId: []
+  },
+  methods: {
+   disAppear() {
+    this.lostHeroId = this.heroes.slice.sort(() => 0.5 - Math.random()).slice(0, Math.ceil(this.heroes.lengyh / 2))
+     .map(hero => hero.id);
+   } 
+  }
+}
+</script>
+<style scoped>
+
+</style>
