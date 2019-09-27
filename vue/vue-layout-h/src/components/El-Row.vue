c57342119a66ceb2bc7df7460ecac85dc58ecc17
yanduanyin <1097720997@qq.com>
2019-09-27 21:45:37 +0800

    vue-layout-h

---
+++ vue/vue-layout-h/src/components/El-Row.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  props: {
+        tag: {
+          type: String, default: 'div'
+        },
+        jiange: {
+          type: Number
+        }
+      },
+//  <template>
+  // <div class="el-row">
+  //   <slot></slot>
+  // </div>
+// </template>
+  render (h) {
+    // h === createElement
+    return h (
+      this.tag,
+      {
+        class: ['el-row']
+      },
+      this.$slots.default
+    ) 
+  }
+}
+</script>
+<style scoped>
+.el-row {
+  width: 100%;
+  display: flex;
+}
+</style>
