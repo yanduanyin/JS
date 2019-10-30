861d60f3ec10e37a18d5ec6eadb81d6a5dc39ad0
yanduanyin <1097720997@qq.com>
2019-10-30 17:14:21 +0800

    vue-mixin

---
+++ vue/vue-mixin/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <my-dialog />
+    <my-loading />
+  </div>
+</template>
+<script>
+import MyDialog from './MyDialog'
+import MyLoading from './MyLoading'
+export default {
+  data () {
+    return {
+    }
+  },
+  components: {
+    MyDialog,
+    MyLoading
+  }
+}
+</script>
+<style scoped>
+
+</style>
