5b32054d6855406b97438f1527b86ff97b450887
yanduanyin <1097720997@qq.com>
2019-11-08 23:14:19 +0800

    know-cars

--- vue/vue-know-cars/src/components/home_page/hot-news.vue
+++ vue/vue-know-cars/src/components/home_page/hot-news.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="news-wrapper">
-    
+
   </div>
 </template>
 
@@ -10,6 +10,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="stylus" scoped>
+.news-wrapper
 
 </style>
