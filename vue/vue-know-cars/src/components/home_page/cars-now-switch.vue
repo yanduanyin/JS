574702d17158cfdd3a7f11573a7c570219b797f8
yanduanyin <1097720997@qq.com>
2019-11-12 12:10:11 +0800

    know cars

--- vue/vue-know-cars/src/components/home_page/cars-now-switch.vue
+++ vue/vue-know-cars/src/components/home_page/cars-now-switch.vue
@@ -9,11 +9,14 @@
             </a>
           </div>
         </div>
-        <div class="swiper-pagination" ref="pagination"></div>
-        <div class="swiper-button-next button">
+        <div class="swiper-pagination" ref="pagination">
+          <span :class="['swiper-pagination-bullet', {'active':currentIndex === index}]"
+           v-for="(item, index) in cirArr" :key="index" @click="clickCirN(index)"></span>
+        </div>
+        <div class="swiper-button-next button" @click="clickNext">
           <span></span>
         </div>
-        <div class="swiper-button-prev button">
+        <div class="swiper-button-prev button" @click="clickPrev">
           <span></span>
         </div>
       </div>
@@ -57,6 +60,7 @@ export default {
       ],
       rate:8, //一张图片的切换速度， 单位为px
       timer: null,//初始化一个定时器
+      cirArr: [],
       picN: 0,//当前显示的图片下标
       cirN: 0,//当前显示图片的小圆点下标
       url: [
@@ -72,100 +76,125 @@ export default {
     this.$nextTick(() => {
       this.$refs.parent.appendChild(this.$refs.parent.children[0].cloneNode(true));//克隆第一张图片至列表尾部
       this.$refs.parent.style.width = this.$refs.parent.children.length * 800 + 'px' // 将父容器的宽度根据内容动态添加
+      //添加小圆点，之所用js添加小圆点，是因为小圆点的数量是由图片张数决定的。
+      let len = this.$refs.item.length
+      for(let i = 0; i < len;i++){
+        this.cirArr.push(i);
+      }
       this.autoRun()
     })
 
   },
+  computed: {
+    currentIndex() {
+      return this.cirN
+    }
+  },
   methods: {
-    Roll(distance){ //参数distance：滚动的目标点（必为图片宽度的倍数）
-      // this.autoRun()
+    RollNext(distance){ //参数distance：滚动的目标点（必为图片宽度的倍数）
       if (this.picN > 1) {
-        console.log('1111')
-        let img_parent_box = this.$refs.parent
-      clearInterval(img_parent_box.timer);  //每次运行该函数必须清除之前的定时器！
+        // console.log('1111')
+        this.autoRun()
+      } else if (this.picN === 0) {
+        this.autoRun()
+      } else if (this.picN === 1) {
+        // console.log(this.$refs.parent, '====55555')
+        this.$refs.parent.style.left = 0;//改变left至真正的第一项处，这个过程是时间太快太短所以可以忽略不计然后立刻
+        this.autoRun()
+      }
+       let img_parent_box = this.$refs.parent
+      clearInterval(img_parent_boxTime);  //每次运行该函数必须清除之前的定时器！
       var speed = img_parent_box.offsetLeft < distance ?  this.rate : (0-this.rate);//判断图片移动的方向  此处用了三元运算符  ？ 前面的不等式成立时为rate,不成立时为0-rete
           // console.log(speed)
-      img_parent_box.timer = setInterval(function(){ //设置定时器，每隔10毫秒，调用一次该匿名函数
+      let img_parent_boxTime = setInterval(function(){ //设置定时器，每隔10毫秒，调用一次该匿名函数
           img_parent_box.style.left = img_parent_box.offsetLeft + speed + "px";//每一次调用滚动到的地方 (速度为 speed px/5 ms)       offsetLeft为元素边框外侧到父元素边框内侧的距离    
           var leave = distance - img_parent_box.offsetLeft;//距目标点剩余的px值      
           /*接近目标点时的处理，滚动接近目标时直接到达， 避免rate值设置不当时不能完整显示图片*/
           // Math.abs()  abs() 方法可返回一个数的绝对值
           // console.log(speed);
-          
           if (Math.abs(leave) <= Math.abs(speed)) {                    
-              clearInterval(img_parent_box.timer);
+              clearInterval(img_parent_boxTime);
               img_parent_box.style.left = distance + "px";
           } 
       },3);
+    },
+    RollPrev(distance){ //参数distance：滚动的目标点（必为图片宽度的倍数）
       this.autoRun()
-      } else if (this.picN === 1) {
-        // clearInterval()
-        console.log(this.$refs.parent, '====55555')
-        this.$refs.parent.style.left = 0;//改变left至真正的第一项处，这个过程是时间太快太短所以可以忽略不计然后立刻
-        let img_parent_box = this.$refs.parent
-      clearInterval(img_parent_box.timer);  //每次运行该函数必须清除之前的定时器！
+      let img_parent_box = this.$refs.parent
+      clearInterval(img_parent_boxTime);  //每次运行该函数必须清除之前的定时器！
       var speed = img_parent_box.offsetLeft < distance ?  this.rate : (0-this.rate);//判断图片移动的方向  此处用了三元运算符  ？ 前面的不等式成立时为rate,不成立时为0-rete
-          // console.log(speed)
-      img_parent_box.timer = setInterval(function(){ //设置定时器，每隔10毫秒，调用一次该匿名函数
-          img_parent_box.style.left = img_parent_box.offsetLeft + speed + "px";//每一次调用滚动到的地方 (速度为 speed px/5 ms)       offsetLeft为元素边框外侧到父元素边框内侧的距离    
+      let img_parent_boxTime = setInterval(function(){ //设置定时器，每隔10毫秒，调用一次该匿名函数
+          img_parent_box.style.left = img_parent_box.offsetLeft + speed + "px";//每一次调用滚动到的地方 (速度为 speed px/5 ms) offsetLeft为元素边框外侧到父元素边框内侧的距离    
           var leave = distance - img_parent_box.offsetLeft;//距目标点剩余的px值      
-          /*接近目标点时的处理，滚动接近目标时直接到达， 避免rate值设置不当时不能完整显示图片*/
-          // Math.abs()  abs() 方法可返回一个数的绝对值
-          // console.log(speed);
-          
           if (Math.abs(leave) <= Math.abs(speed)) {                    
-              clearInterval(img_parent_box.timer);
+              clearInterval(img_parent_boxTime);
               img_parent_box.style.left = distance + "px";
           } 
-      },4);
-        this.autoRun()
-      }
+      },3);
     },
     autoRun(){
-      //添加小圆点，之所用js添加小圆点，是因为小圆点的数量是由图片张数决定的。
       let len = this.$refs.item.length
-      for(let i = 0; i < len;i++){
-        var ele_span = document.createElement('span');//创建span元素节点
-        // ele_span.className = 'quiet';//给li添加类名quiet
-        this.$refs.pagination.appendChild(ele_span);
-      }
       this.picN = ++this.picN
-      // cirN++
       //如果轮播完克隆项应该轮播回第二张照片上，因为克隆项和第一张图片一样
-      // console.log( this.$refs.item.length, '222----')
-      // console.log(this.$refs.item)
-      console.log(this.picN)
+      // console.log(this.picN)
       // 自动轮播，当图片为第一张时应该自动到第二张上去，所以要传入第二张的picN值，以次类推
-      if (this.picN !== len+1) {
-        setTimeout( () => {
-          this.Roll(-this.picN*800)
-        }, 3500)
-      } else {
+      if (this.picN == len+1) {
         this.picN = 1
-        console.log('left至真正的第一项处')
-        console.log(this.picN)
-        setTimeout( () => {
-          this.Roll(-this.picN*800)
-        }, 3500)
+        // console.log('left至真正的第一项处')
+        // console.log(this.picN)
       }
-            
-      //判断是否到了最后一个圆点，当圆点到了最后一个时，应该变回第一个点进行轮播
-      // if (cirN > this.$refs.item.length - 1) {
-      //     cirN = 0;
-      // }
-      // for(var i = 0; i < this.$refs.item.length;i++) {
-      //     cLis[i].className = 'quiet';//让所有圆点背景色变为默认色
-      // }
-      // cLis[cirN].className = 'active';
+      this.timer = setTimeout( () => {
+        this.RollNext(-this.picN*800)
+        this.cirN == this.cirN++
+        if (this.cirN == this.$refs.item.length) {
+          //判断是否到了最后一个圆点，当圆点到了最后一个时，应该变回第一个点进行轮播
+            this.cirN = 0;
+        } 
+      }, 3500)
+    },
+    clickCirN(index) {
+      clearTimeout(this.timer)
+      this.$refs.parent.style.left = -800*(index) + 'px';//改变left至真正的第一项处，这个过程是时间太快太短所以可以忽略不计然后立刻
+      this.cirN = index
+      this.picN = index
+      this.autoRun()
+    },
+    clickNext() {
+      clearTimeout(this.timer)
+      ++this.cirN
+      // console.log(this.cirN, '++++');
+      if (this.cirN == this.$refs.item.length) {
+        //判断是否到了最后一个圆点，当圆点到了最后一个时，应该变回第一个点进行轮播
+          this.cirN = 0;
+      } 
+      this.RollNext(-this.picN*800)
+    },
+    clickPrev() {
+      clearTimeout(this.timer)
+      --this.cirN
+      if (this.cirN == -1) {
+        //判断是否到了最后一个圆点，当圆点到了最后一个时，应该变回第一个点进行轮播
+          this.cirN = this.$refs.item.length - 1;
+      }
+      if (this.picN == 1) {
+        this.picN = this.$refs.item.length - 1
+        this.$refs.parent.style.left = -800*(this.picN+1) + 'px';
+        this.RollNext(-this.picN*800)
+      } else if (this.picN === 3){
+        this.picN = --this.picN
+        this.picN = --this.picN
+        this.RollPrev(-this.picN*800)
+      } else {
+        this.picN = --this.picN
+        this.picN = --this.picN
+        this.RollNext(-this.picN*800)
+      }
     }
-    //注意：以上轮播图片时是当图片索引值picN已经有了，要+1，等待一定时间执行移动到下一张图片的事件
-    // 开始自动滚动：
-
   }
 }
 </script>
 
-<style lang="stylus" scoped>
+<style lang="stylus">
 .wrapper
   margin-top 10px
   .wrapper-switch
@@ -207,23 +236,23 @@ export default {
               background-size: cover;
               overflow: hidden;
       .swiper-pagination
+        line-height 20px
         bottom: 0px
         left: 0
         width: 100%
         position: absolute
         text-align: center
         -webkit-transition: 300ms opacity
-        -o-transition: 300ms opacity
         transition: 300ms opacity
         -webkit-transform: translate3d(0, 0, 0)
         transform: translate3d(0, 0, 0)
         z-index: 10
         .swiper-pagination-bullet
+          display: inline-block
           margin: 0 4px
           cursor: pointer
           width: 8px
           height: 8px
-          display: inline-block
           border-radius: 100%
           background-color: #e6e6e6
         .active
@@ -254,9 +283,11 @@ export default {
           left: 50%
           margin-top: -16px
           margin-left: -9.5px
-          background-image: url('data:image/pngbase64,iVBORw0KGgoAAAANSUhEUgAAACYAAABACAYAAAB2kAXpAAAAAXNSR…gopigYWqskGpkLiqHiBgdK+hdC/h27h6hZvELDnkPr2wN/baRBstNmdUsAAAAASUVORK5CYII=')
+          background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADkAAABgCAYAAACnmdq6AAAAAXNSR0IArs4c6QAAAkhJREFUeAHtnD9OwzAYxVsGFipGrsGGxMIKV+IYPQA3gBUhISExMbKwoi6dqeAC4blqrNTYsaPa8uePZ8mq/zX2z34viSrXsxkDZ0DPDHRdd2aiHqIdCaAWiEvENWIfTNqULZoHBsQ54goxFFaouGgWFIM/RvxAjIVvNLhsEhQDv43RDerbBAXAywAiJdkeKKi+UsicNuJBjxxfbZx8SvYUjZ4ALtajLuR7CpWnjXhQO2asxrUjxalZ8dLdwoLqbiqZ014+KAZ8gvjqDHxqlqDWH7UTXNFp4qV0ayvW9k/pUrreGaBHrUdqJ+hRr0KDhZRubcXa/indoEq9FZSulU7tBKXrVWiwkNKtrVjbP6UbVKm3gtK10qmdoHS9Cg0WUrq1FWv7p3SDKvVWULpWOrUTlK5XocHCfyVd2btSMkl3hevI3meUCXRZ+14T7T8D6DraiYQGGUD39gK6GyMkMOofQ4ZV/CNXUStpALGMj4hXByznwwHfLfvVDCuIS2x3d8p8hGQCNG89Ml8GMgLK3PZGQMxAQpD7Yo7B6971RcAEfaIJJVr2aT5ydUqUEt3OAD044pKyVfQgPUgPlvVY7Or0ID1ID8ZcUraeHqQH6cGyHotdnR6kB+nBmEvK1tODrXvQ6AMMuv+4DUD9f8EH5H2aGr2t5P6qNryFYeif3uHHC9sA3PlR5QEnw4U0Nx2VR9W4uz+e96jHMz+ovpnP52/jzYTVYiX1Hx9l5hygug8C64UFUN1HuvWg/SeAdR7O1wPykzMgawZ+ARwclMWJw6EYAAAAAElFTkSuQmCC')
           background-repeat: no-repeat
           background-size: cover
+        &:hover
+          opacity: 0.4
       .swiper-button-prev
         left 0
         border-radius 0 8px 8px 0
@@ -273,6 +304,8 @@ export default {
           background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACYAAABACAYAAAB2kAXpAAAAAXNSR0IArs4c6QAAAgJJREFUaAXt2kFKxTAQBuBX8RTiwht4heIJBJfuBM+gqFvBO/hw51Jx6Up6hXcDF+IphPqPZEpsX9sknZlk8QJ9ado08zGPlqbtarUrcRmo4rrL9W7b9gCjHbsRN1VVffujm8MAopiXWC6w7DvMD+o1cGvXXpnCHOoWwU8Z0KvvgHunbXu9HWrNABTFPmeACSwQRaYjM1gEikxfJrBIFJle1GEJqA91WAKqAeoGZ2TLGRO/XCSiroGia1lXRGFSKNKJwSRRYjBplAhMA7UYpoVaBNNEJcO0UUkwC1Q0zAoVBbNEBcOsUUGwHKhZWC7UJCwnahSWG7UVVgJqACsFRbD+9I1myGOTUervlwaNwZ2n32HJenejiGzRs4RXLDxtnxq3wU41FAX2M0YPOIpA9WHULqb4GdtA9W+mMqKssf0Bf31IdkeGmN/cwTB9oudTT/OH/PWo8auK62AO9Ij6za3PVTU6qOG6s5IVpVzLBjACloDbCisBNwrLjZuE5cTNwnLhgmA5cMEwa1wUzBIXDbPCJcEscMkwbdwimCZuMUwLJwLTwInBpHGiMEmcOEwKpwJLxNFLrit+n9S/56cxRYoLcI/BQucQJ+h7xsHVYBQgAWcDS8AdmmSMg0Rk7pOPUf0rOQjVgbhnPkbtrOQA/dpNDcv6IMRHAjj5CY3fd7cekoFfCpo+slA7IxYAAAAASUVORK5CYII=');
           background-repeat: no-repeat;
           background-size: cover;
+        &:hover
+          opacity: 0.4
   .today-news
     width: 380px
     height: 316px
