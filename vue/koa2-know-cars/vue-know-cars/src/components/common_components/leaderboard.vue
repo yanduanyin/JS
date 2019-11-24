248a4fb3e0bf8fe9d123d3df3f4fe9f438e19824
yanduanyin <1097720997@qq.com>
2019-11-24 23:53:41 +0800

    knowcars

--- vue/koa2-know-cars/vue-know-cars/src/components/common_components/leaderboard.vue
+++ vue/koa2-know-cars/vue-know-cars/src/components/common_components/leaderboard.vue
@@ -1,24 +1,45 @@
 <template>
-  <div class="hotspot">
-    <div class="header">
-      <span class="title">{{LbName}}</span>
-      <span class="more">
-        总榜单
-        <span class="more-icon iconfont icon-jiantouarrow487"></span>
-      </span>
-    </div>
-    <div class="list">
-      <div class="series clearfix">
-        <span class="icon" v-if="Ranking" style="background-image:url('http://localhost:8080/static/know-cars-image/0_img.png')"></span>
-        <span class="month"  v-else></span>
-        <a href="" class="cars-name" target="_blank" title="">
-          <span class="photo"></span>
-          <span class="name line-1">沃尔沃S60</span>
-          <span class="active price">19.05万起</span>
-        </a>
+  <div>
+    <div class="hotspot">
+      <div class="header">
+        <span class="title">{{LbName}}</span>
+        <span class="more">
+          总榜单
+          <span class="more-icon iconfont icon-jiantouarrow487"></span>
+        </span>
       </div>
-    </div>
-  </div>  
+      <div class="list">
+        <div class="series clearfix">
+          <span class="icon" v-if="Ranking" style="background-image:url('http://localhost:8080/static/know-cars-image/0_img.png')"></span>
+          <span class="month"  v-else></span>
+          <a href="" class="cars-name" target="_blank" title="">
+            <span class="photo"></span>
+            <span class="name line-1">沃尔沃S60</span>
+            <span class="active price">19.05万起</span>
+          </a>
+        </div>
+      </div>
+    </div>  
+    <div class="newcar">
+      <div class="header">
+        <span class="title">{{LbName}}</span>
+        <span class="more">
+          总榜单
+          <span class="more-icon iconfont icon-jiantouarrow487"></span>
+        </span>
+      </div>
+      <div class="list">
+        <div class="series clearfix">
+          <span class="month"></span>
+          <a href="" class="cars-name" target="_blank" title="">
+            <span class="photo"></span>
+            <span class="name line-1">沃尔沃S60</span>
+            <span class="active price">19.05万起</span>
+          </a>
+        </div>
+      </div>
+    </div>  
+  </div>
 </template>
 
 <script>
