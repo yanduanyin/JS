8ed9f3ed9675b860f3d7509a56f56ea9b55c3cfa
yanduanyin <1097720997@qq.com>
2019-12-11 10:56:39 +0800

    data

--- js-module/require.js
+++ js-module/require.js
@@ -1,5 +1,5 @@
 var { counter, obj, changeValue} = 
-require('./node-common.js');
+require('./export.js');
 
 // 3  {}
 console.log(counter, obj);
@@ -7,8 +7,8 @@ changeValue();
 // 3 变了
 console.log(counter, obj);
 // 复制
-// var a = 1; var b = a; a = 2;
-// var aa = {}; var bb = aa; aa.name = 'bbname';
+var a = 1; var b = a; a = 2; b = 5;
+var aa = {}; var bb = aa; aa.name = 'bbname';
 // commonJS require 的时候 类比 js 
 // 基本数据类型 复杂数据类型 的复制,
 // 它也就是 复制了一份导出来的东西
